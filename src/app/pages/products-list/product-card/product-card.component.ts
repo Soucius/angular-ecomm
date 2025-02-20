7fad46e1c121e3746da904021c5e4318bf91da3d
Soucius <soucius.ggwp@gmail.com>
2025-02-20 21:19:46 +0300

    adding out of stock indicator

--- src/app/pages/products-list/product-card/product-card.component.ts
+++ src/app/pages/products-list/product-card/product-card.component.ts
@@ -17,6 +17,14 @@ import { PrimaryButtonComponent } from "../../../components/primary-button/prima
 
           <app-primary-button label="Add to Cart" class="mt-3" />
         </div>
+
+        <span class="absolute top-2 right-3 text-sm font-bold">
+          @if (product().stock) {
+            {{ product().stock }} left
+          } @else {
+            Out of stock
+          }
+        </span>
       </div>
     </div>
   `,
